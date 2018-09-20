0e736950e80f43df43810eb738e28630fcb4f458
Mace Muilman <mace@macemuilman.nl>
2018-09-20 23:21:57 +0200

    Initial commit.

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+docker build -t mace015/laravel-docker-application ../images/application
+docker build -t mace015/laravel-docker-workspace ../images/workspace
+docker build -t mace015/laravel-docker-worker ../images/worker
