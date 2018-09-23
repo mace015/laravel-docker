6ee83365914761b2b813d6bc2dfdbb03a75d03c9
Mace Muilman <mace@macemuilman.nl>
2018-09-23 21:42:10 +0200

    Added the cron image to the build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 docker build -t mace015/laravel-docker-application ../images/application
 docker build -t mace015/laravel-docker-workspace ../images/workspace
 docker build -t mace015/laravel-docker-worker ../images/worker
+docker build -t mace015/laravel-docker-cron ../images/cron
