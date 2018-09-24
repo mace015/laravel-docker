08128da974562c9b2127ad0807f84997ef335788
Mace Muilman <mace@macemuilman.nl>
2018-09-24 19:49:12 +0200

    Included the 2 new base images in the build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+docker build -t mace015/laravel-docker-php-fpm $DIR/../images/php-fpm
+docker build -t mace015/laravel-docker-php-cli $DIR/../images/php-cli
+
 docker build -t mace015/laravel-docker-application $DIR/../images/application
 docker build -t mace015/laravel-docker-workspace $DIR/../images/workspace
 docker build -t mace015/laravel-docker-worker $DIR/../images/worker
