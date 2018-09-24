aa596ceb54a8ad913d368504bf5b595be7f95724
Mace Muilman <mace@macemuilman.nl>
2018-09-24 15:56:45 +0200

    Made the build script callable from anywhere.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
-docker build -t mace015/laravel-docker-application ../images/application
-docker build -t mace015/laravel-docker-workspace ../images/workspace
-docker build -t mace015/laravel-docker-worker ../images/worker
-docker build -t mace015/laravel-docker-cron ../images/cron
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+docker build -t mace015/laravel-docker-application $DIR/../images/application
+docker build -t mace015/laravel-docker-workspace $DIR/../images/workspace
+docker build -t mace015/laravel-docker-worker $DIR/../images/worker
+docker build -t mace015/laravel-docker-cron $DIR/../images/cron
